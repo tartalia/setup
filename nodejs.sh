0d1c54f3487a032dd9399989e69e3fe680988e88
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-05-27 00:54:18 -0300

    Fixing nodejs installation issues

--- nodejs.sh
+++ nodejs.sh
@@ -2,12 +2,15 @@
 # NodeJS setup
 
 # Install nvm: node-version manager
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.3/install.sh | sudo sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
 nvm install v0.12.4
 nvm use v0.12.4
+
+# Reinstall curl
+#curl -L https://npmjs.org/install.sh | sudo sh
 
 # Install jshint to allow checking of JS code within emacs
 sudo npm install -g jshint
