ba702b933d92c4bb2137b44d772ce753bb4ca7dc
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-05-27 01:02:22 -0300

    Fixing nodejs installation issues

--- nodejs.sh
+++ nodejs.sh
@@ -9,8 +9,10 @@ source $HOME/.nvm/nvm.sh
 nvm install v0.12.4
 nvm use v0.12.4
 
-# Reinstall curl
-#curl -L https://npmjs.org/install.sh | sudo sh
+sudo ln -s /usr/local/bin/node /usr/bin/node
+sudo ln -s /usr/local/lib/node /usr/lib/node
+sudo ln -s /usr/local/bin/npm /usr/bin/npm
+sudo ln -s /usr/local/bin/node-waf /usr/bin/node-waf
 
 # Install jshint to allow checking of JS code within emacs
 sudo npm install -g jshint
