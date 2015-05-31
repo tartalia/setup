82cf005090960d4327a8de2ae080f4a297e94c89
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-05-31 19:19:20 -0300

    Added zencoding-mode to emacs dotfiles

--- dotfiles-emacs/.emacs.d/init.el
+++ dotfiles-emacs/.emacs.d/init.el
@@ -16,7 +16,7 @@
 (require 'whitespace)
 (require 'dired-x)
 (require 'compile)
-(require 'zencoding-mode)
+
 (ido-mode t)
 (menu-bar-mode -1)
 (normal-erase-is-backspace-mode 1)
@@ -142,3 +142,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(js-indent-level 2))
+
+;; Zencoding
+(add-to-list 'load-path "~/.emacs.d/zencoding")
+(require 'zencoding-mode)
