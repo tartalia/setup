784c21732ca464d9fc0af70bdc2a46ad97e7822b
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-06-14 19:27:03 -0300

    modified postgres install script

--- postgresql.sh
+++ postgresql.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # Setup postgresql
-sudo apt-get install postgresql postgresql-contrib
+sudo apt-get update
+sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3
+sudo apt-get install -y postgresql-server-dev-9.3
 sudo apt-get install -y pgadmin3-dbg
+
+# Create postgresql cluste
 
