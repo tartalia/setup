0d67fdcf7445b2318ac9c5d8c3d3ea67b4666ca4
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-06-14 16:25:57 -0300

    modified postgresql install

--- postgresql.sh
+++ postgresql.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Emacs setup
 
 # Setup postgresql
-sudo apt-get install -y postgresql postgresql-contrib 
-sudo apt-get install -y postgresql-server-dev-9.1
+sudo apt-get install postgresql postgresql-contrib
 sudo apt-get install -y pgadmin3-dbg
 
